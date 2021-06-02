c58f9f672092a9ff87f95f08055da388fb75c4b1
Dmitry Kh <dmitry.kh@msp360.com>
2021-06-02 20:51:53 +0300

    Count

--- src/main/java/com/levelup/application.java
+++ src/main/java/com/levelup/application.java
@@ -2,7 +2,11 @@ package com.levelup;
 
 public class application {
     public static void main(String[] args) {
+
+        Integer count = 0;
         System.out.println("Hello world again, motherfuckers");
         System.out.println("Pull this");
+        count++;
+        System.out.println(count);
     }
 }
