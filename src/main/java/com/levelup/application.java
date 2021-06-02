980956f80bb451aac566613e227bdcfb9fa72e11
Dmitry Kh <dmitry.kh@msp360.com>
2021-06-02 20:40:16 +0300

    Hello world 2

--- src/main/java/com/levelup/application.java
+++ src/main/java/com/levelup/application.java
@@ -3,5 +3,6 @@ package com.levelup;
 public class application {
     public static void main(String[] args) {
         System.out.println("Hello world again, motherfuckers");
+        System.out.println("Pull this");
     }
 }
